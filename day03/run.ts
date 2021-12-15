ab7644e6c964bcb0c1cec0b4c12a418a955e42ff
Sebastian Forman <sebastian@forman.no>
2021-12-15 19:22:35 +0100

    Day 3 - part 2

--- day03/run.ts
+++ day03/run.ts
@@ -2,6 +2,8 @@ import { readFileSync } from "fs";
 
 const DAY = "Day 03";
 
+type bit = "0" | "1";
+
 const loadInput = (): string[] => {
   const input = readFileSync(__dirname + "/input.txt", { encoding: "utf8" });
   const inputArray = input.split(/\r\n/);
@@ -37,9 +39,53 @@ const part1 = (input: string[]): number => {
   return result;
 };
 
+const part2 = (input: string[]): number => {
+  const findMostCommonBit = (
+    values: string[],
+    index: number,
+    oxygen: boolean
+  ): bit => {
+    let on = 0;
+    let off = 0;
+
+    for (let i = 0; i < values.length; i++) {
+      values[i].charAt(index) === "0" ? off++ : on++;
+    }
+
+    if (oxygen) {
+      return on >= off ? "1" : "0";
+    }
+    return on >= off ? "0" : "1";
+  };
+
+  const binarySize = input[0].length;
+  let oxygenCriteria: string[] = [...input];
+  let co2Criteria: string[] = [...input];
+  let oxygen: string;
+  let co2: string;
+
+  for (let i = 0; i < binarySize; i++) {
+    const oxBit = !oxygen && findMostCommonBit(oxygenCriteria, i, true);
+    const co2Bit = !co2 && findMostCommonBit(co2Criteria, i, false);
+
+    oxygenCriteria = oxygenCriteria.filter((item) => item.charAt(i) === oxBit);
+    co2Criteria = co2Criteria.filter((item) => item.charAt(i) === co2Bit);
+
+    if (oxygenCriteria.length === 1) oxygen = oxygenCriteria[0];
+    if (co2Criteria.length === 1) co2 = co2Criteria[0];
+  }
+
+  const oxygenRating = parseInt(oxygen, 2);
+  const co2Rating = parseInt(co2, 2);
+  const result = oxygenRating * co2Rating;
+  console.log(DAY, "- part 2", result);
+  return result;
+};
+
 const run = () => {
   const input = loadInput();
   part1(input);
+  part2(input);
 };
 
 export default run;
