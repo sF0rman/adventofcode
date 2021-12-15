4f6780414b3a0f5e4102ff77e60f77c2f1d19ca3
Sebastian Forman <sebastian@forman.no>
2021-12-15 18:00:14 +0100

    Day 2 - part 2

--- day02/run.ts
+++ day02/run.ts
@@ -10,6 +10,7 @@ interface instructionSet {
 interface position {
   depth: number;
   distance: number;
+  aim?: number;
 }
 
 const DAY = "Day 02";
@@ -31,24 +32,46 @@ const part1 = (input: instructionSet[]): number => {
   for (let i = 0; i < input.length; i++) {
     switch (input[i].dir) {
       case "forward":
-        position.distance = position.distance + input[i].val;
+        position.distance += input[i].val;
         break;
       case "up":
-        position.depth = position.depth - input[i].val;
+        position.depth -= input[i].val;
         break;
       case "down":
-        position.depth = position.depth + input[i].val;
+        position.depth += input[i].val;
         break;
     }
   }
   const result = position.depth * position.distance;
-  console.log(DAY, "- part 1", result);
+  console.log(DAY, "- part 1:", result);
+  return result;
+};
+
+const part2 = (input: instructionSet[]): number => {
+  let position: position = { depth: 0, distance: 0, aim: 0 };
+  for (let i = 0; i < input.length; i++) {
+    switch (input[i].dir) {
+      case "forward":
+        position.distance += input[i].val;
+        position.depth += input[i].val * position.aim;
+        break;
+      case "up":
+        position.aim -= input[i].val;
+        break;
+      case "down":
+        position.aim += input[i].val;
+        break;
+    }
+  }
+  const result = position.depth * position.distance;
+  console.log(DAY, "- part 2:", result);
   return result;
 };
 
 const run = (): void => {
   const input = loadInput();
   part1(input);
+  part2(input);
 };
 
 export default run;
