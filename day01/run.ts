3b2856a3e1ae69703ba375fe8db7fc6b14ca4510
Sebastian Forman <sebastian@forman.no>
2021-12-15 17:18:46 +0100

    Day 1 - part 2

--- day01/run.ts
+++ day01/run.ts
@@ -1,25 +1,37 @@
 import { readFileSync } from "fs";
 
+const DAY = "Day 01";
+
 const loadInput = (): number[] => {
   const input = readFileSync(__dirname + "/input.txt", { encoding: "utf8" });
   const inputArray = input.split(/\r\n/).map((item) => parseInt(item));
   return inputArray;
 };
 
-const calculate = (input: number[]): number => {
+const part1 = (input: number[]): number => {
   let result = 0;
   for (let i = 1; i < input.length; i++) {
-    if (input[i - 1] < input[i]) {
-      result++;
-    }
+    input[i - 1] < input[i] && result++;
   }
+  console.log(DAY, "- part 1:", result);
+  return result;
+};
+
+const part2 = (input: number[]): number => {
+  let result = 0;
+  for (let i = 3; i < input.length; i++) {
+    const previous = input[i - 3] + input[i - 2] + input[i - 1];
+    const current = input[i - 2] + input[i - 1] + input[i];
+    current > previous && result++;
+  }
+  console.log(DAY, "- part 2", result);
   return result;
 };
 
 const run = () => {
   const input = loadInput();
-  const result = calculate(input);
-  console.log(result);
+  part1(input);
+  part2(input);
 };
 
 export default run;
